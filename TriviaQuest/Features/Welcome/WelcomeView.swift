39abb289874f4d36084e9293b2acbcaedc1013de
Jake Gordon <jakegordon53@gmail.com>
2024-04-07 20:58:06 +0100

    - added-app-icon - disabled-landscape - disabled-dark-mode

--- TriviaQuest/Features/Welcome/WelcomeView.swift
+++ TriviaQuest/Features/Welcome/WelcomeView.swift
@@ -11,8 +11,14 @@ struct WelcomeView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Image("brain")
-                Text("Trivia Quest").padding(EdgeInsets(top: 30, leading: 20, bottom: 30, trailing: 20)).font(Font.custom("New", size: 48))
+                ZStack {
+                    Image("brain").resizable()
+                        .frame(width: 125, height: 125)
+                    Image("glasses").resizable()
+                        .frame(width: 98, height: 98)
+                }.padding(EdgeInsets(top: 100, leading: 0, bottom: 0, trailing: 0))
+                Text("Trivia Quest").padding(EdgeInsets(top: 5, leading: 20, bottom: 30, trailing: 20)).font(Font.custom("New", size: 48))
+                Spacer()
                 NavigationLink(destination: AllQuestionsView()) {
                     Text("Let's Play!")
                         .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
@@ -21,7 +27,7 @@ struct WelcomeView: View {
                 }.background(
                     RoundedRectangle(cornerRadius: 10)
                         .fill(.red)
-                    )
+                ).padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
 
 //                NavigationLink(destination: MyIssues()) {
 //                    Text("My Issues").padding(EdgeInsets(top: 0, leading: 0, bottom: 40, trailing: 0))
