f41d02143d6134d062f9f5ebfe9a140102bf99e1
Jake Gordon <jakegordon53@gmail.com>
2024-04-08 21:04:39 +0100

    - wrote-passing-code-for-delete-questions-test

--- TriviaQuest/Features/Welcome/WelcomeViewModel.swift
+++ TriviaQuest/Features/Welcome/WelcomeViewModel.swift
@@ -6,8 +6,21 @@
 //
 
 import Foundation
+import CoreData
 
 class WelcomeViewModel: ObservableObject {
     
-    
+    func deleteQuestions(coreDataService: PersistenceModule) {
+        
+        let request: NSFetchRequest<Question> = Question.fetchRequest()
+        do {
+            let questions = try coreDataService.managedObjectContext.fetch(request)
+            for q in questions {
+                coreDataService.managedObjectContext.delete(q as NSManagedObject)
+            }
+            try coreDataService.managedObjectContext.save()
+        } catch {
+            fatalError("Error deleting question objects")
+        }
+    }
 }
